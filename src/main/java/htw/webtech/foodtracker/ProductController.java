0360558b5918f76f42105a88ecd8ccef7ca6b9ca
Beril G <Beril.Guemues@Student.HTW-Berlin.de>
2026-05-22 15:12:52 +0200

    meilenstein 3

--- src/main/java/htw/webtech/foodtracker/ProductController.java
+++ src/main/java/htw/webtech/foodtracker/ProductController.java
@@ -1,5 +1,6 @@
 package htw.webtech.foodtracker;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,10 @@ import java.util.List;
 import java.util.Arrays;
 
 @RestController
-@RequestMapping("/products")
-
+@CrossOrigin(origins = "*") // <--- HIER EINFÜGEN
 public class ProductController {
 
-    @GetMapping
+    @GetMapping("/products")
     public List<Product> getAllProducts() {
         return Arrays.asList(
                 new Product(1, "Vollmilchschokolade", 550, 7.5, 55.0),
@@ -22,6 +22,6 @@ public class ProductController {
                 new Product(6, "Spiegelei", 196, 13.0, 1.4),
                 new Product(7, "Rindersteak", 250, 26.0, 0.0),
                 new Product(8, "Nudeln", 350, 12.0, 71.0)
-        );
+        ); }
     }
-}
+
